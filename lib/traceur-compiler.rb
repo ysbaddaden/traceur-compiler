247e88f1024861052aaec2fc99d95ac7300e8b67
Julien Portalier <julien@portalier.com>
2015-05-19 17:05:40 +0200

    Fix: memoize ExecJS context (increases performance)

--- lib/traceur-compiler.rb
+++ lib/traceur-compiler.rb
@@ -9,10 +9,6 @@ module TraceurCompiler
       @path ||= ENV["TRACEUR_COMPILER_SOURCE_PATH"]
     end
 
-    def self.path=(path)
-      @path = path
-    end
-
     def self.compile_path
       File.expand_path("../traceur-compiler/compile.js", __FILE__)
     end
@@ -22,11 +18,11 @@ module TraceurCompiler
     end
 
     def self.contents
-      @contents = File.read(path) + File.read(compile_path)
+      @contents ||= File.read(path) + File.read(compile_path)
     end
 
     def self.context
-      ExecJS.compile(contents)
+      @context ||= ExecJS.compile(contents)
     end
   end
 
